f0a074d94d5f817ae154f28edf03ca72a75121c7
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-01 00:03:40 -0300

    books route at search controller working as expected with full api documentation

--- Api/Controllers/SearchesController.cs
+++ Api/Controllers/SearchesController.cs
@@ -20,19 +20,64 @@ namespace Api.Controllers
             _context = context;
         }
 
-        // GET api/Searches/5
-        [HttpGet("books/by/category/{categoryName}")]
-        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooksByCategory(string categoryName)
+        // GET: api/Searches/books?categoryName={string}&authorName={string}&bookTitle={string}
+        /// <summary>
+        /// Get books by category name(string), author name(string) or book title(string), or by a combination of all of those.
+        /// </summary>
+        /// <returns>Get books by category name(string), author name(string) or book title(string), or by a combination of all of those.</returns>
+        /// <remarks>
+        /// Instructions: Just send a GET request to URI /api/searches/books?{params}, where "params" could be : 
+        ///     categoryName={string}
+        ///     authorName={string}
+        ///     bookTitle={string}
+        ///     or using "&amp;" to concatenate:
+        ///     
+        ///     categoryName={string}&amp;authorName={string}&amp;bookTitle={string}
+        /// </remarks>
+        /// <param name="categoryName" example="Tech"></param>
+        /// <param name="authorName" example="Maycon"></param>
+        /// <param name="bookTitle" example="Borland"></param>
+        /// <response code="200">Book list returned successfully</response>
+        /// <response code="404">Book list not found.</response>
+        [HttpGet("books")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooksByCategory(string? categoryName, string? authorName, string? bookTitle)
         {
-            if (_context.Categories == null)
+            if (_context.Categories == null || _context.Authors == null || _context.Books == null)
             {
                 return NotFound();
             }
 
-            return await _context.Books
-                .Where(cn => cn.Category.Name == categoryName)
+            var query = _context.Books.AsQueryable();
+
+            if (categoryName != null)
+            {
+                query = query.Where(cn => cn.Category.Name.Contains(categoryName));
+            }
+
+            if (authorName != null)
+            {
+                query = query.Where(cn => cn.Author.Name.Contains(authorName));
+            }
+
+            if (bookTitle != null)
+            {
+                query = query.Where(cn => cn.Title.Contains(bookTitle));
+            }
+
+
+            List<BookDTO> result = await query
                 .Select(b => BookToDTO(b))
                 .ToListAsync();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return result;
         }
 
         //// POST api/Searches
