afc9f6d0cae280fd93898683c3c7b78cdcfdad92
Rafael Pereira Dias <p.rafael.dias@viveo.com.br>
2023-04-25 21:22:56 -0300

    início da refatoração. DDD, camadas e repositório.

---
+++ Library.Entities/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Entities
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public bool IsBillingAddress { get; set; }
+        public ICollection<Billing> Billings { get; set; }
+        public User User { get; set; }
+    }
+}
